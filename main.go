df51fb3cd298b10595e5397b1e563baa389aa4ea
Kyle Erf <erf@mongodb.com>
2014-07-26 23:46:43 -0400

    Version 0.9

--- main.go
+++ main.go
@@ -12,10 +12,17 @@ func Two() int {
 }
 
 func main() {
-	flag.Parse()
 	uff := unused.NewUnusedFunctionFinder()
-	uff.Verbose = true
-	//uff.IncludeAll = true
+	flag.BoolVar(&(uff.Verbose), "v", false,
+		"prints extra information during execution to stderr")
+	flag.BoolVar(&(uff.IncludeAll), "all", false,
+		"includes all found packages in analysis, not just main packages")
+	flag.StringVar(&(uff.Ignore), "ignore", "",
+		"don't read files that match the given string (use to avoid /testdata, etc) ")
+	flag.StringVar(&(uff.CallgraphJSON), "callgraphjson", "",
+		"pass in a callgraph in json format instead of computing one")
+	flag.Parse()
+
 	unusedFuncs, err := uff.Run(flag.Args())
 	if err != nil {
 		os.Exit(1)
--- unused/unused_func_finder.go
+++ unused/unused_func_finder.go
@@ -5,7 +5,7 @@ package unused
 import (
 	"code.google.com/p/go.tools/oracle"
 	"code.google.com/p/go.tools/oracle/serial"
-	//"encoding/json"
+	"encoding/json"
 	"fmt"
 	"go/ast"
 	"go/build"
@@ -26,14 +26,16 @@ type FoundFunc struct {
 type UnusedFuncFinder struct {
 	Callgraph []serial.CallGraph
 
+	Ignore        string
 	Verbose       bool
 	IncludeAll    bool
 	LogWriter     io.Writer
-	CallgraphJSON []byte // for setting user json input (hack?)
+	CallgraphJSON string // for setting user json input (hack?)
 
 	filesByCaller map[string][]string
 	pkgs          map[string]struct{}
 	funcs         []FoundFunc
+	numFilesRead  int
 }
 
 func NewUnusedFunctionFinder() *UnusedFuncFinder {
@@ -77,7 +79,7 @@ func (uff *UnusedFuncFinder) pkgsAsArray() []string {
 	return packages
 }
 
-func (uff *UnusedFuncFinder) getCallgraphJSONFromOracle() error {
+func (uff *UnusedFuncFinder) getCallgraphFromOracle() error {
 	res, err := oracle.Query(uff.pkgsAsArray(), "callgraph", "", nil, &build.Default, true)
 	if err != nil {
 		return err
@@ -90,7 +92,22 @@ func (uff *UnusedFuncFinder) getCallgraphJSONFromOracle() error {
 	return nil
 }
 
+func (uff *UnusedFuncFinder) getCallgraphFromJSON(input io.Reader) error {
+	serialRes := serial.Result{}
+	dec := json.NewDecoder(input)
+	err := dec.Decode(&serialRes)
+	if err != nil {
+		return fmt.Errorf("error reading json: %v", err)
+	}
+	if len(serialRes.Callgraph) == 0 {
+		return fmt.Errorf("no callgraph found in json input")
+	}
+	uff.Callgraph = serialRes.Callgraph
+	return nil
+}
+
 func (uff *UnusedFuncFinder) readFuncsAndImportsFromFile(filename string) error {
+
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, filename, nil, 0)
 	if err != nil {
@@ -128,6 +145,7 @@ func (uff *UnusedFuncFinder) readFuncsAndImportsFromFile(filename string) error 
 		return true
 	})
 
+	uff.numFilesRead++
 	return nil
 }
 
@@ -183,6 +201,10 @@ func getFullPkgName(filename string) (string, error) {
 }
 
 func (uff *UnusedFuncFinder) canReadSourceFile(filename string) bool {
+	if uff.Ignore != "" && strings.Contains(filename, uff.Ignore) { //TODO regex
+		uff.Logf("Ignoring path '%v'", filename)
+		return false
+	}
 	if !strings.HasSuffix(filename, ".go") {
 		return false
 	}
@@ -207,7 +229,18 @@ func (uff *UnusedFuncFinder) readDir(dirname string) error {
 
 func (uff *UnusedFuncFinder) Run(fileArgs []string) ([]FoundFunc, error) {
 
+	// do some basic sanity checks on system configuration
+	if len(fileArgs) == 0 {
+		uff.Errorf("Must supply at least one file as an argument")
+		return nil, fmt.Errorf("no files supplied as arguments")
+	}
+	if os.Getenv("GOPATH") == "" {
+		uff.Errorf("GOPATH environment varaible is not set")
+		return nil, fmt.Errorf("GOPATH not set")
+	}
+
 	// first, get all the file names and package imports
+	uff.Logf("Collecting func declarations from source files")
 	for _, filename := range fileArgs {
 		if isDir(filename) {
 			if err := uff.readDir(filename); err != nil {
@@ -223,24 +256,37 @@ func (uff *UnusedFuncFinder) Run(fileArgs []string) ([]FoundFunc, error) {
 			}
 		}
 	}
+	uff.Logf("Parsed %v source files", uff.numFilesRead)
 
 	// then get the callgraph from json or the oracle
-	if uff.CallgraphJSON == nil {
+	if uff.CallgraphJSON == "" {
 		uff.Logf("Running callgraph analysis on following packages: \n\t%v",
 			strings.Join(uff.pkgsAsArray(), "\n\t"))
-		if err := uff.getCallgraphJSONFromOracle(); err != nil {
+		if err := uff.getCallgraphFromOracle(); err != nil {
 			uff.Errorf("Error getting results from oracle: %v", err.Error())
 			return nil, err
 		}
 	} else {
-		//TODO json parser
+		uff.Logf("Reading callgraph from '%v'", uff.CallgraphJSON)
+		jsonFile, err := os.Open(uff.CallgraphJSON)
+		if err != nil {
+			uff.Errorf("Error opening json file: %v", err.Error())
+			return nil, err
+		}
+		err = uff.getCallgraphFromJSON(jsonFile)
+		if err != nil {
+			uff.Errorf("Error reading callgrapg from json file: %v", err.Error())
+			return nil, err
+		}
 	}
 
 	// use that callgraph to build a callgraph->file map
 	uff.buildFileMap()
 
 	// finally, figure out which functions are not in the graph
+	uff.Logf("Scanning callgraph for unused functions")
 	unusedFuncs := uff.computeUnusedFuncs()
 
+	uff.Logf("") // assure space between log output and results
 	return unusedFuncs, nil
 }
