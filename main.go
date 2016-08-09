8dc03c090400416630cefad715284e94d5f61ad5
Kyle Erf <erf@mongodb.com>
2016-08-09 14:06:14 -0500

    kind parsing

--- main.go
+++ main.go
@@ -11,8 +11,16 @@ import (
 	"strings"
 )
 
+var validKinds = map[string]struct{}{
+	unused.Miscs:      struct{}{},
+	unused.Functions:  struct{}{},
+	unused.Variables:  struct{}{},
+	unused.Parameters: struct{}{},
+	unused.Fields:     struct{}{},
+}
+
 func main() {
-	var ignoreList string
+	var ignoreList, kindList string
 	var ungrouped, shortNames bool
 	ucf := unused.NewUnusedCodeFinder()
 	flag.BoolVar(&(ucf.Verbose), "v", false,
@@ -21,14 +29,31 @@ func main() {
 	flag.BoolVar(&ungrouped, "ungrouped", false, "disable output type grouping")
 	flag.BoolVar(&shortNames, "shortnames", false, "display identifiers with simpler syntax")
 	flag.StringVar(&ignoreList, "ignore", "",
-		"don't read files that contain the given comma-separated strings (use to avoid /testdata, etc) ")
+		"don't read files that contain these comma-separated strings (use to avoid /testdata, etc)")
+	flag.StringVar(&kindList, "kinds", "",
+		"comma-separated string of kinds of unused code to report (funcs,vars,fields,params,misc)")
 	// hack for testing code with build flags
 	flag.Var((*buildutil.TagsFlag)(&build.Default.BuildTags), "tags", "a list of build tags")
 	flag.Parse()
+
 	// handle ignore list
 	ucf.Ignore = strings.Split(ignoreList, ",")
 	if len(ucf.Ignore) > 0 && ucf.Ignore[0] == "" {
 		ucf.Ignore = nil
+	}
+
+	kinds := []string{unused.Functions, unused.Variables, unused.Fields, unused.Parameters}
+	// parse and validate kind list
+	if kindList != "" {
+		kinds = strings.Split(kindList, ",")
+		for _, k := range kinds {
+			if _, ok := validKinds[k]; !ok {
+				fmt.Printf(
+					"ERROR: '%v' is not a valid kind. "+
+						"Valid kinds are funcs, vars, fields, params, or misc.\n", k)
+				os.Exit(2)
+			}
+		}
 	}
 
 	args := flag.Args()
@@ -42,40 +67,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	sort.Sort(unused.ByPosition(unusedObjs))
+	ucf.Logf("Sorting and grouping %v results for types %v", len(unusedObjs), strings.Join(kinds, ","))
+	grouping := unused.GroupObjects(unusedObjs)
 	if ungrouped {
-		// TODO types should work without grouping
-		ucf.Logf("Printing %v results, ungrouped\n", len(unusedObjs)) // ensure a newline before printing results if -v is on
-		for _, o := range unusedObjs {
-			if shortNames {
-				fmt.Println(unused.ObjectString(o))
-			} else {
-				fmt.Println(unused.ObjectFullString(o))
-			}
+		ucf.Logf("Printing results withobjs grouping\n", len(unusedObjs))
+		objs := []unused.Object{}
+		for _, k := range kinds {
+			objs = append(objs, grouping[k]...)
 		}
+		sort.Sort(unused.ByPosition(objs))
+		printObjs(shortNames, objs)
 	} else {
-		grouping := []string{unused.Functions, unused.Variables, unused.Fields, unused.Parameters}
-		ucf.Logf("Printing %v results for types %v\n",
-			len(unusedObjs), strings.Join(grouping, ","))
-		printGrouped(
-			shortNames,
-			grouping, //FIXME
-			unusedObjs)
+		ucf.Logf("Printing results with grouping\n")
+		for _, k := range kinds {
+			fmt.Println("[", strings.ToUpper(k), "]")
+			objs := grouping[k]
+			sort.Sort(unused.ByPosition(objs))
+			printObjs(shortNames, objs)
+			fmt.Println()
+		}
 	}
 }
 
-func printGrouped(shortNames bool, types []string, objs []unused.Object) {
-	grouped := unused.GroupObjects(objs)
-	// todo use list
-	for _, t := range types {
-		fmt.Println("[", strings.ToUpper(t), "]")
-		for _, o := range grouped[t] {
-			if shortNames {
-				fmt.Println(unused.ObjectString(o))
-			} else {
-				fmt.Println(unused.ObjectFullString(o))
-			}
+func printObjs(shortNames bool, objs []unused.Object) {
+	for _, o := range objs {
+		if shortNames {
+			fmt.Println(unused.ObjectString(o))
+		} else {
+			fmt.Println(unused.ObjectFullString(o))
 		}
-		fmt.Println()
 	}
 }
